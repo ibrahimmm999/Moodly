276ad78173369da8acb9b7404f0a9cbe25c690b3
Ibrahim Imam <ibrhmmm552@gmail.com>
2022-12-28 20:59:46 +0700

    update pubspec, theme, assets

---
+++ lib/shared/theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = const Color(0xffF54769);
+Color secondaryColor = const Color(0xff82C3EC);
+Color dark = const Color(0xff002347);
+Color grey = const Color(0xff9698A9);
+Color white = const Color(0xffFFFFFF);
+Color white2 = const Color(0xffFAFAFA);
+
+TextStyle primaryColorText = GoogleFonts.poppins(color: primaryColor);
+TextStyle greyText = GoogleFonts.poppins(color: grey);
+TextStyle secondaryColorText = GoogleFonts.poppins(color: secondaryColor);
+TextStyle whiteText = GoogleFonts.poppins(color: white);
+TextStyle darkText = GoogleFonts.poppins(color: dark);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
