95a751f2a2ae5c1b667819e1ea511192be70e6dc
ardhanurfan <ardhanurfan@gmail.com>
2023-01-01 15:59:47 +0700

    update

--- lib/service/article_service.dart
+++ lib/service/article_service.dart
@@ -4,26 +4,6 @@ import 'package:moodly/models/article_model.dart';
 class ArticleService {
   final CollectionReference _articlesReference =
       FirebaseFirestore.instance.collection('articles');
-  Future<List<ArticleModel>> fetchArticles() async {
-    try {
-      QuerySnapshot result = await _articlesReference.get();
-      List<ArticleModel> articles = result.docs.map((e) {
-        return ArticleModel.fromJson(e.id, e.data() as Map<String, dynamic>);
-      }).toList();
-      return articles;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<ArticleModel> getArticleById(String id) async {
-    try {
-      DocumentSnapshot snapshot = await _articlesReference.doc(id).get();
-      return ArticleModel.fromJson(id, snapshot.data() as Map<String, dynamic>);
-    } catch (e) {
-      rethrow;
-    }
-  }
 
   Future<void> addArticle(ArticleModel article) async {
     try {
